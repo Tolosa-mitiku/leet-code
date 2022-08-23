689db41d5e21f8c971a33ccdf0c0b7b10770368e
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-08-23 11:40:25 +0300

    Time: 1051 ms (68.65%), Space: 131.1 MB (5.06%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        def helper(node):
+            nonlocal head
+            if not node.next:
+                return node.val == head.val
+            temp = helper(node.next)
+            head = head.next
+            return temp and (head.val == node.val)
+        return helper(head)
+        
