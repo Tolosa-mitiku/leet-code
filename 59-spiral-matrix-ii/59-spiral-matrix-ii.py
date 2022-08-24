600a4e255c3ccac1f68f84f751e24774e20d7512
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-08-25 00:23:10 +0300

    Time: 52 ms (46.64%), Space: 13.9 MB (83.07%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
         ans = [[-1 for i in range(n)] for i in range(n)]
-        visited = set()
         x = 0
         y = 0
         dx, dy = 1, 0
         count = 0
         while count < n * n:
             ans[y][x] = count + 1
-            visited.add((y, x))
-            if x + dx < 0 or x + dx >= n or y + dy < 0 or y + dy >= n or (y + dy, x + dx) in visited:
+            if x + dx < 0 or x + dx >= n or y + dy < 0 or y + dy >= n or ans[y + dy][x + dx] != -1:
                 dx, dy = -1 * dy, dx
             x += dx
             y += dy
