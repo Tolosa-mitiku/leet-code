542f984919636db3dd935cc76dd2fe17f5baa17f
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-08-25 15:14:34 +0300

    Time: 83 ms (20.72%), Space: 13.8 MB (77.34%) - LeetHub

---
+++ 1854-maximum-population-year/1854-maximum-population-year.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        ans = [0] * 101
+        for b, d  in logs:
+            ans[b-1950] += 1
+            ans[d-1950] -= 1
+        maxx = 0
+        count = 0
+        curr = 0
+        for i in range(len(ans)):
+            curr += ans[i]
+            if curr > count:
+                count = curr
+                maxx = i
+        return maxx+1950
