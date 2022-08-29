d011b97cc9478c4c1980deaa077eecc380f1f687
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-08-29 11:22:57 +0300

    Time: 1495 ms (87.99%), Space: 26.9 MB (93.69%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -2,7 +2,6 @@ class Solution:
     def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
         bound = lambda row, col : 0 <= row < len(land) and 0 <= col < len(land[0])
         Adj = [[0,1], [1,0]]
-        visited = set()
         
         ans = []
         def helper(x, y):
@@ -14,13 +13,13 @@ class Solution:
                 maxx = max(maxx, (row, col))
                 minn = min(minn, (row, col))
                 for nrow, ncol in Adj:
-                    if (nrow+row, ncol+col) not in visited and bound(nrow+row, ncol+col) and land[nrow+row][ncol+col] == 1:
+                    if bound(nrow+row, ncol+col) and land[nrow+row][ncol+col] == 1:
                         queue.append((nrow+row, ncol+col))
-                    visited.add((nrow+row, ncol+col))
+                        land[nrow+row][ncol+col] = 0
             return [minn[0], minn[1], maxx[0], maxx[1]]
         for i in range(len(land)):
             for j in range(len(land[0])):
-                if land[i][j] and (i, j) not in visited:
+                if land[i][j]:
                     ans.append(helper(i, j))
         return ans
                     
