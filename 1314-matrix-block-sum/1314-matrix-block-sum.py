203047d24c5dc4938db44c20626d65bfa15261f5
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-09-19 23:34:55 +0300

    Time: 240 ms (29.38%), Space: 15.4 MB (8.41%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -1,34 +1,23 @@
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        secmatrix = [[False for i in range(len(mat[0])+1)] for j in range(len(mat)+1)]
+        secmatrix = [[0 for i in range(len(mat[0])+1)] for j in range(len(mat)+1)]
         for i in range(len(mat)):
-            prev = 0
             for j in range(len(mat[0])):
-                prev += mat[i][j]
-                secmatrix[i+1][j+1] = prev
-                mat[i][j] = prev
-        for i in range(len(mat[0])):
-            prev = 0
-            for j in range(len(mat)):
-                prev += mat[j][i]
-                secmatrix[j+1][i+1] = prev
+                secmatrix[i+1][j+1] += mat[i][j] + secmatrix[i+1][j] + secmatrix[i][j+1] - secmatrix[i][j]
+        
         def helper(row1, col1):
             if row1 < 0:
-                row = 0
-            elif row1 >= len(mat):
-                row = len(mat)
-            else:
-                row = row1
+                row1 = 0
+            elif row1 > len(mat):
+                row1 = len(mat)
             if col1 < 0:
-                col = 0
-            elif col1 >= len(mat[0]):
-                col = len(mat[0])
-            else:
-                col = col1
-            return secmatrix[row][col]
-        ans = [[False for i in range(len(mat[0]))] for j in range(len(mat))]
+                col1 = 0
+            elif col1 > len(mat[0]):
+                col1 = len(mat[0])
+            return secmatrix[row1][col1]
+        
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                ans[i][j] = helper(i+k+1, j+k+1) + helper(i-k, j-k) - helper(i-k, j+k+1) - helper(i+k+1, j-k)
-        return ans
+                mat[i][j] = helper(i+k+1, j+k+1) + helper(i-k, j-k) - helper(i-k, j+k+1) - helper(i+k+1, j-k)
+        return mat
                 
