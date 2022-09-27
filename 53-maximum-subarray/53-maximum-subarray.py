55102cce4b8f00e1d8aff9c6f5d5ac456cfedac2
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-09-28 00:17:01 +0300

    Time: 1061 ms (65.25%), Space: 27.9 MB (39.83%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -5,6 +5,4 @@ class Solution:
         for i in range(1, len(nums)):
             curr = max(curr + nums[i], nums[i])
             ans = max(ans, curr)
-        ans = max(ans, curr)
-            
         return ans
