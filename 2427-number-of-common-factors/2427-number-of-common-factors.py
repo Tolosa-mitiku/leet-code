18bf749c888a611f29e1989053209fa21a527f1b
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-04 06:37:28 -0700

    Time: 37 ms (85.97%), Space: 13.9 MB (62.40%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,11 +1,7 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
-        afactors = set()
-        bfactors = set()
-        for potentialfactor in range(1, a+1):
-            if a % potentialfactor == 0:
-                afactors.add(potentialfactor)
-        for potentialfactor in range(1, b+1):
-            if b % potentialfactor == 0:
-                bfactors.add(potentialfactor)
-        return len(afactors.intersection(bfactors))
+        ans = 0
+        for potentialfactor in range(1, min(a,b)+1):
+            if a % potentialfactor == 0 and b % potentialfactor == 0:
+                ans += 1
+        return ans
