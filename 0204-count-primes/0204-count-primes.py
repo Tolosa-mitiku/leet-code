6bffd36148cfe15f00cfe90e177af6f5ff09c9c5
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-04 05:50:38 -0700

    Time: 8924 ms (10.07%), Space: 67.8 MB (47.90%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -6,7 +6,7 @@ class Solution:
         p = 2
         while p * p <= n:
             if prime[p]:
-                for i in range(2 * p,n+1,p):
+                for i in range(p * p,n+1,p):
                     prime[i] = False
             p += 1
         return sum(prime) - 2
