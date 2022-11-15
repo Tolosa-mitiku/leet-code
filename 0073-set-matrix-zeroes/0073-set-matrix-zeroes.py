e6c4ed85c0b4709abef0c732b145c4171df2b66d
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-15 17:42:08 +0300

    Time: 302 ms (46.33%), Space: 14.9 MB (16.62%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,16 +3,35 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row = set()
-        col = set()
+        
+        row = False
+        col = False
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                col = True
+        for j in range(len(matrix[0])):
+            if matrix[0][j] == 0:
+                row = True
+        
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    row.add(i)
-                    col.add(j)
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in row or j in col:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+                    
+        for i in range(1, len(matrix)):
+            if matrix[i][0] == 0:
+                for j in range(len(matrix[0])):
                     matrix[i][j] = 0
-        
+        for j in range(1, len(matrix[0])):
+            if matrix[0][j] == 0:
+                for i in range(len(matrix)):
+                    matrix[i][j] = 0
+                    
+        if row:
+            for j in range(len(matrix[0])):
+                matrix[0][j] = 0
+        if col:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
         
