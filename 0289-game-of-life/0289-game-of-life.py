e8f7c9f6afe8c3fa6db8bf47437151ccadba4a2a
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-16 12:15:18 +0300

    Time: 64 ms (48.51%), Space: 13.8 MB (99.81%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,27 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        
+        adj = [[0,1],[1,0],[0,-1],[-1,0],[1,1],[1,-1],[-1,1],[-1,-1]]
+        
+        def inbound(row,col):
+            return 0 <= row < len(board) and 0 <= col < len(board[0])
+        sett = set()
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                count = 0
+                for x, y in adj:
+                    if inbound(i+x, j+y) and board[i+x][j+y] == 1:
+                        count += 1
+                if board[i][j] == 1 and (count < 2 or count > 3):
+                    sett.add((i,j))
+                elif board[i][j] == 0 and count == 3:
+                    sett.add((i,j))
+        for i, j in sett:
+            if board[i][j]:
+                board[i][j] = 0
+            else:
+                board[i][j] = 1
+                    
