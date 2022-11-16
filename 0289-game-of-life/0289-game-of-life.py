4f7295042e3cda593ec95ecf7c4f1d496594e05a
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-16 12:22:29 +0300

    Time: 44 ms (78.64%), Space: 13.9 MB (91.72%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -8,20 +8,20 @@ class Solution:
         
         def inbound(row,col):
             return 0 <= row < len(board) and 0 <= col < len(board[0])
-        sett = set()
         for i in range(len(board)):
             for j in range(len(board[0])):
                 count = 0
                 for x, y in adj:
-                    if inbound(i+x, j+y) and board[i+x][j+y] == 1:
+                    if inbound(i+x, j+y) and (board[i+x][j+y] == 1 or board[i+x][j+y] == -2):
                         count += 1
                 if board[i][j] == 1 and (count < 2 or count > 3):
-                    sett.add((i,j))
+                    board[i][j] = -2
                 elif board[i][j] == 0 and count == 3:
-                    sett.add((i,j))
-        for i, j in sett:
-            if board[i][j]:
-                board[i][j] = 0
-            else:
-                board[i][j] = 1
+                    board[i][j] = 2
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == -2:
+                    board[i][j] = 0
+                elif board[i][j] == 2:
+                    board[i][j] = 1
                     
