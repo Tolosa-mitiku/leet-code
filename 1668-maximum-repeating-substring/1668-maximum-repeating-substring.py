2243cb6019d171253f4c1fa8ef5cbd0a90509c5c
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-16 22:36:24 +0300

    Time: 65 ms (25.76%), Space: 13.9 MB (64.19%) - LeetHub

--- 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
+++ 1668-maximum-repeating-substring/1668-maximum-repeating-substring.py
@@ -1,24 +1,7 @@
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
         
-        def checker(i, j):
-            count = 0
-            for k in range(i, j):
-                if j > len(sequence) or sequence[k] != word[count]:
-                    return False
-                count += 1
-            return True
-        
-        ans = 0
-        count = 0
-        for i in range(len(word)):
-            count = 0
-            for j in range(i,len(sequence), len(word)):
-                if checker(j, j + len(word)):
-                    count += 1
-                else:
-                    ans = max(ans, count)
-                    count = 0
-            ans = max(ans, count)
-        return ans
-                    
+        ans = 1
+        while ans * word in sequence:
+            ans += 1
+        return ans - 1
