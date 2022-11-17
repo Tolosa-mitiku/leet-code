99e307d3b04465381b7f2b87359b151255a290c9
Tolosa-mitiku <61460697+Tolosa-mitiku@users.noreply.github.com>
2022-11-17 11:50:50 +0300

    Time: 202 ms (13.19%), Space: 26.4 MB (32.11%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -6,18 +6,15 @@
 #         self.right = None
 
 class Solution:
+    def helper(self, node, pq):
+        if not node:
+            return (False, None)
+        left = self.helper(node.left, pq)
+        right = self.helper(node.right, pq)
+        ans = None
+        if sum([left[0], right[0], node in pq]) == 2:
+            ans = node
+        return (left[0] or right[0] or (node in pq), ans or left[1] or right[1])
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.helper(root, [p,q])[1]
         
-        pq = [p,q]
-        ans = 0
-        def helper(node):
-            nonlocal ans
-            if not node:
-                return False
-            left = helper(node.left)
-            right = helper(node.right)
-            if sum([left, right, node in pq]) == 2:
-                ans = node
-            return left or right or (node in pq)
-        helper(root)
-        return ans
