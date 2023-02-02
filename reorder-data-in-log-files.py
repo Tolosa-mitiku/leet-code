fcd9b64f069c0ec9013a590ee470a9de3ddda1ae
Tolosa Mitiku <se.tolosa.mitiku@gmail.com>
2023-02-02 17:47:27 +0300

    reorder-data-in-log-files

---
+++ reorder-data-in-log-files.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        digits = []
+        letters = []
+        for i in range(len(logs)):
+            log = logs[i].split()
+            if log[1].isdigit():
+                digits.append(logs[i])
+            else:
+                letters.append(logs[i])
+        
+        letters.sort(key=lambda x : (x.split()[1:], x.split()[0]))
+        return letters + digits
